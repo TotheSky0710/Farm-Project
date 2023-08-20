d28b115cf4ad386ef078118874c38df33642a4e9
TotheSky0710 <steeve.dev.0710@outlook.com>
2023-08-21 01:33:36 +0900

    route update

--- backend/routes/customerRoutes.js
+++ backend/routes/customerRoutes.js
@@ -5,27 +5,16 @@ const auth = require("../middleware/auth");
 const router = express.Router();
 
 router.get("/", auth, customerController.getCustomer);
-
 router.patch("/", auth, customerController.updateCustomer);
-
 router.post("/register", customerController.registerCustomer);
-
 router.post("/login", customerController.loginCustomer);
-
 router.post("/addToCart", auth, customerController.addToCart);
-
 router.post("/removeFromCart", auth, customerController.removeFromCart);
-
 router.post("/updateCart", auth, customerController.updateCart);
-
 router.post("/addSub", auth, customerController.addSubscription);
-
 router.post("/removeSub", auth, customerController.removeSubscription);
-
 router.get("/getSubs", auth, customerController.getSubscriptions);
-
 router.get("/getCart", auth, customerController.getCart);
-
 router.get("/getVendors", customerController.getVendors);
 
 module.exports = router;
